f2c741e67bb98701d91eec274947375465de1d61
Daniel <danielgondimdecastro@gmail.com>
2025-11-10 20:33:29 -0300

    feat: adiciona validações aos atributos
    - NotNulll para validação Java
    - COlumn(nullable = false) para não permitir salvar no banco vazio
    - CreationTimestamp e UpdateTimestamp para atualizar automaticamente as datas

--- src/main/java/com/salvalivros/backend/model/Livro.java
+++ src/main/java/com/salvalivros/backend/model/Livro.java
@@ -1,7 +1,10 @@
 package com.salvalivros.backend.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -14,17 +17,31 @@ public class Livro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Column(nullable = false)
     private String titulo;
 
+    @NotNull
+    @Column(nullable = false)
     private String autor;
 
+    @NotNull
+    @Column(nullable = false)
     private String editora;
 
+    @NotNull
+    @Column(unique = true)
     private String isbn;
 
+    @CreationTimestamp
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 
+    @NotNull
+    @Column(nullable = false)
     private String categoria;
 }

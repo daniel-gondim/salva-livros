8caffa4b13ac4f166d0480ddafec58a11725296b
Daniel <danielgondimdecastro@gmail.com>
2025-11-14 23:37:57 -0300

    test: implementa teste de busca por Id
     - implementa método de criação de livro para massa de dados

--- src/test/java/com/salvalivros/backend/repository/LivroRepositoryTest.java
+++ src/test/java/com/salvalivros/backend/repository/LivroRepositoryTest.java
@@ -5,21 +5,41 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class LivroRepositoryTest {
 
     @Autowired
     LivroRepository livroRepository;
 
-    @Test
-    void deveSalvarLivro() {
+    private Livro criaLivro() {
         Livro livro = new Livro();
         livro.setAutor("Autor");
         livro.setCategoria("Categoria Teste");
         livro.setTitulo("Autor Teste");
         livro.setEditora("Editora Teste");
         livro.setIsbn("12313132121231");
+        return livro;
+    }
+
+    @Test
+    void deveSalvarLivro() {
+        Livro livro = criaLivro();
         livroRepository.save(livro);
         assert livro.getId() != null;
+    }
+
+    @Test
+    void deveBuscarLivroPorId() {
+        Livro livro = criaLivro();
+        livroRepository.save(livro);
+        Optional<Livro> livroEncontrado = livroRepository.findById(livro.getId());
+        assert livroEncontrado.isPresent();
+
+        Livro encontrado = livroEncontrado.get();
+        assertEquals("Autor Teste", encontrado.getTitulo());
     }
 }
